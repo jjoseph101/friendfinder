4a00d3d55787f575daaa2de22f756c7257a77ed8
JimmyJ <jimmyj.inbox@gmail.com>
2017-02-10 18:27:43 -0600

    set up server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// dependencies
+var body-parser = require("body-parser");
+var path = require("path");
+var express = require("express");
+
+//creating express server
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//interpret data to server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+//routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//listener
+app.listen(PORT, function () {
+	console.log("App listening on PORT: " + PORT);
+});
