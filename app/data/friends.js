f14ac96200fa99423ebf0140829a15e93ca115bb
JimmyJ <jimmyj.inbox@gmail.com>
2017-02-10 21:11:05 -0600

    basic structure

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,16 +5,19 @@ var friendArray = [
 		"name":"Rocky",
 		"photo":"https://shechive.files.wordpress.com/2010/01/dork-is-cool-14.jpg",
 		"scores":[
-			5,
-			4,
-			3,
-			2,
-			1,
-			1,
-			2,
-			3,
-			4,
-			5
+			"5",
+			"4",
+			"3",
+			"2",
+			"1",
+			"1",
+			"2",
+			"3",
+			"4",
+			"5"
 		]
 	}
-]
+];
+
+//export array
+module.exports = friendArray;
